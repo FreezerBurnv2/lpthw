979040ecf565153b385a5f39e05cce7aaae9a403
Jeff Long <blacktone@gmail.com>
2013-05-02 14:15:45 -0400

    Ex3 Study Drill 3
    Create a .py with some calc you need to do.

--- ex3.py
+++ ex3.py
@@ -1,23 +1,36 @@
-print "I will now count my chickens:"
+# States the Action
+print "I will now count my chickens:"    
+# This adds 25 and 30 then divides by 6
+print "Hens", 25.0 + 30 / 6      
+# I read why it works but I'm still confused about the percent.          
+print "Roosters", 100.0 - 25 * 3 % 4       
+# Text noting Line 8's calculation
+print "Now I will count the eggs:"       
+# Calculation from line 6
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6  
+# States the caluclation for line 12
+print "Is it true that 3 + 2 < 5 - 7?"   
+# Calculation from line 10
+print 3.0 + 2 < 5 - 7                      
+# prints string then comma to display result on same line
+print "What is 3 + 2?", 3.0 + 2  
+# same as the line above (These calculations are to show why line 10 is correct.          
+print "What is 5 - 7?", 5.00 - 7            
+# another string
+print "Oh, that's why it's False."       
+# another string
+print "How about some more."             
+# showing use of greater-than
+print "Is it greater?", 5.0 > -2        
+   # showing use of greater-than-equal
+print "Is it greater or equal?", 5.0 >= -2 
+# showing use of less-than-equal
+print "Is it less or equal?", 5.0 <= -2    
 
-print "Hens", 25 + 30 / 6 #This adds 25 and 30 then divides by 6
-print "Roosters", 100 - 25 * 3 % 4 #This subtracts 25 from 100 then multiplies 
+# Study Drills
 
-print "Now I will count the eggs:"
-
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
-print "Is it true that 3 + 2 < 5 - 7?"
-
-print 3 + 2 < 5 - 7
-
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
-
-print "Oh, that's why it's False."
-
-print "How about some more."
-
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+#1 Above each line, use the # to write a comment to yourself explaining what the line does.
+#2 Remember in Ex 0 when you started python? Start python this way again and using above characters and what you know, use python as a calculator
+#3 Find something you need ot calculate and write a new .py file that does it.
+#4 Notice the math seems "wrong"? There are no fractions, only whole numbers. Find out why by researching what a "floating point" number is.
+#5 Rewrite ex3.py to use floating point numbers so it's more accurate (hint: 20.0 is floating point)
---
+++ ex3_studydrill3.py
@@ -0,0 +1,11 @@
+print "*****Python Extra Credit*****"
+
+print "How many keys are on my key chain?"
+
+print "Keys I have that are mine", 5
+print "Keys I have that are company keys", 5
+
+a=5 #number of keys that are mine
+b=5 #number of keys that are company keys
+
+print "Add the keys together", a + b
