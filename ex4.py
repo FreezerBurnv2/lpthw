89fcd41b64895b7c6aa4d6cda5ccfba78afd7d21
Jeff Long <blacktone@gmail.com>
2013-05-02 19:19:17 -0400

    Ex4

--- ex4.py
+++ ex4.py
@@ -10,6 +10,22 @@ average_passengers_per_car = passengers / cars_driven
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
 print "There will be", cars_not_driven, "empty cars today."
-print "There will be", cars_not_driven, "empty cars today."
-print "We can transport", carpool_capacity, "people today."
+# Deleted copied line 12
+print "We can transport", carpool_capacity, "people today." #correction
 print "We need to put about", average_passengers_per_car, "in each car."
+
+
+#Study Drill
+
+#Error explained - Seems he forgot to either put in "car_pool_capacity" or mispelled it.
+
+print "****************** Test Program ****************************"
+
+print "Why does 4.0 + 5", 4.0 + 5
+
+A = 4.0
+B = 5
+
+print "Why does A + B", A * B
+
+print "But when I do this", carpool_capacity
